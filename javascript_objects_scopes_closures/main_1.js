bed42feb6c7b521dc67f15abaa48115bcb92531e
maleda mesfin <markanmesfin555@gmail.com>
2024-02-01 15:27:12 +0300

    creat file

--- javascript_objects_scopes_closures/main_1.js
+++ javascript_objects_scopes_closures/main_1.js
@@ -1,14 +1,19 @@
+// File: 2-rectangle.js
+
 class Rectangle {
     constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        } else {
-            // Set width and height to undefined
-            this.width = undefined;
-            this.height = undefined;
+      if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+        // Create an empty object only if both w and h are not positive integers or are 0
+        if (w === undefined || h === undefined) {
+          return {};
         }
+      } else {
+        // Initialize instance attributes width and height
+        this.width = w;
+        this.height = h;
+      }
     }
-}
-
-module.exports = Rectangle;
+  }
+  
+  module.exports = Rectangle;
+  
